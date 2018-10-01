35f478d3c6bbabff06d7b8fa296f330e74061039
Ramon Recuero <ramon@oly.ai>
2018-10-01 14:01:40 -0700

    Website loads and uses sample contract

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,53 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import ReadString from "./ReadString";
+import SetString from "./SetString";
 import './App.css';
 
 class App extends Component {
+  state = { loading: true, drizzleState: null };
+
+  componentDidMount() {
+    const { drizzle } = this.props;
+
+    // subscribe to changes in the store
+    this.unsubscribe = drizzle.store.subscribe(() => {
+
+      // every time the store updates, grab the state from drizzle
+      const drizzleState = drizzle.store.getState();
+
+      // check to see if it's ready, if so, update local component state
+      if (drizzleState.drizzleStatus.initialized) {
+        this.setState({ loading: false, drizzleState });
+      }
+    });
+  }
+
+  compomentWillUnmount() {
+    this.unsubscribe();
+  }
+
   render() {
+    if (this.state.loading) {
+      return "Loading Drizzle...";
+    }
     return (
       <div className="App">
+        <h1> Drizzle is ready </h1>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App-intro">
+          <ReadString
+            drizzle={this.props.drizzle}
+            drizzleState={this.state.drizzleState}
+          />
+          <SetString
+            drizzle={this.props.drizzle}
+            drizzleState={this.state.drizzleState}
+          />
+        </div>
       </div>
     );
   }
--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+// import drizzle functions and contract artifact
+import { Drizzle, generateStore } from "drizzle";
+import MyStringStore from "./contracts/MyStringStore.json";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// let drizzle know what contracts we want
+const options = { contracts: [MyStringStore] };
+
+// setup the drizzle store and drizzle
+const drizzleStore = generateStore(options);
+const drizzle = new Drizzle(options, drizzleStore);
+
+
+ReactDOM.render(<App drizzle={drizzle} />, document.getElementById('root'));
 registerServiceWorker();

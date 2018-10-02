8542ae482be6a6115d8cf0d5599bfbd6c0b991ff
Ramon Recuero <ramon@oly.ai>
2018-10-02 14:01:02 -0700

    Router

--- client/src/index.js
+++ client/src/index.js
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './utils/registerServiceWorker';
+import { createBrowserHistory } from 'history';
 
 // import drizzle functions and contract artifact
 import { Drizzle, generateStore } from "drizzle";
+import { DrizzleContext } from "drizzle-react";
+import { LoadingContainer } from 'drizzle-react-components';
 import MyStringStore from "./contracts/MyStringStore.json";
 
 // import css
@@ -18,6 +21,21 @@ const options = { contracts: [MyStringStore] };
 // setup the drizzle store and drizzle
 const drizzleStore = generateStore(options);
 const drizzle = new Drizzle(options, drizzleStore);
+const history = createBrowserHistory();
 
 ReactDOM.render(<App drizzle={drizzle} />, document.getElementById('root'));
 registerServiceWorker();
+
+ReactDOM.render((
+    <DrizzleContext.Provider drizzle={drizzle}>
+      <App drizzle={drizzle}>
+        <LoadingContainer>
+          <Router history={history} store={drizzleStore}>
+            <Route exact path="/" component={App} />
+          </Router>
+        </LoadingContainer>
+      </App>
+    </DrizzleContext.Provider>
+  ),
+  document.getElementById('root')
+);
